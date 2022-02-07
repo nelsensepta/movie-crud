0c39e80cf11d6aeab57f425436e9b4feb4a48ac5
nelsesepta <nelseseptahenidar@gmail.com>
2022-02-07 08:47:04 +0700

    Add not-found page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import WatchList from "./pages/WatchList";
 import Details from "./pages/Details";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Routes>
       <Route index path="/" element={<Home />} />
-      <Route index path="/watchlist" element={<WatchList />} />
-      <Route path="/movie/:id" element={<Details />} />
+      <Route index path="watchlist" element={<WatchList />} />
+      <Route path="movie/:id" element={<Details />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
---
+++ src/pages/NotFound.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./NotFound.css";
+export default function NotFound() {
+  return (
+    <div className="not-found">
+      <h1 className="not-found-title">Not Found 404</h1>
+    </div>
+  );
+}
